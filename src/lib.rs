26806a5a2a951e8753e0d089407a4fb137b82a7e
Xenely14 <a.maryatkin14@gmail.com>
2023-05-18 23:00:22 +0800

    Add methods to alloc, free memory, show byte table

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,44 @@
 
 const PATH_SIZE: usize = 512;
 
-pub const PROCESS_ALL_ACCESS: u32 = 0x001F0FFF;
+const PROCESS_ALL_ACCESS: u32 = 0x001F0FFF;
+
+pub const MEM_DECOMMIT: u32 = 0x00004000;
+pub const MEM_RELEASE: u32 = 0x00008000;
+pub const MEM_COALESCE_PLACEHOLDERS: u32 = 0x00000001;
+pub const MEM_PRESERVE_PLACEHOLDER: u32 = 0x00000002;
+
+pub const MEM_COMMIT: u32 = 0x00001000;
+pub const MEM_RESERVE: u32 = 0x00002000;
+pub const MEM_RESET: u32 = 0x00080000;
+pub const MEM_RESET_UNDO: u32 = 0x1000000;
+pub const MEM_LARGE_PAGES: u32 = 0x20000000;
+pub const MEM_PHYSICAL: u32 = 0x00400000;
+pub const MEM_TOP_DOWN: u32 = 0x00100000;
+
+pub const PAGE_NOACCESS: u32 = 0x001;
+pub const PAGE_READONLY: u32 = 0x002;
+pub const PAGE_READWRITE: u32 = 0x004;
+pub const PAGE_WRITECOPY: u32 = 0x008;
+pub const PAGE_EXECUTE_READ: u32 = 0x0020;
+pub const PAGE_EXECUTE_READWRITE: u32 = 0x0040;
 
 #[link(name="kernel32")]
 extern "C"
 {
-    pub fn OpenProcess(desired_access: u32, inherit_handle: i32, process_id: u32) -> *const ();
-    pub fn ReadProcessMemory(process_handle: *const (), read_address: *const (), buffer: *const (), buffer_size: usize, bytes_read: *const usize) -> i32;
-    pub fn WriteProcessMemory(process_handle: *const (), write_address: *const (), buffer: *const (), buffer_size: usize, bytes_written : *const usize) -> i32;
-
-    pub fn IsWow64Process(process_handle: *const (), is_64_bit: *const i32) -> i32;
+    fn OpenProcess(desired_access: u32, inherit_handle: i32, process_id: u32) -> *const ();
+    fn ReadProcessMemory(process_handle: *const (), read_address: *const (), buffer: *const (), buffer_size: usize, bytes_read: *const usize) -> i32;
+    fn WriteProcessMemory(process_handle: *const (), write_address: *const (), buffer: *const (), buffer_size: usize, bytes_written : *const usize) -> i32;
+    fn VirtualAllocEx(process_handle: *const (), desired_location: *const (), allocation_size: usize, allocation_type: u32, protection: u32) -> *const ();
+    fn VirtualFreeEx(process_handle: *const (), allocation_address: *const (), allocation_size: usize, free_type: u32) -> i32;
+    fn IsWow64Process(process_handle: *const (), is_64_bit: *const i32) -> i32;
 }
 
 #[link(name="psapi")]
 extern "C"
 {
-    pub fn EnumProcesses(process_ids_list: *const u32, process_ids_list_size: u32, bytes_returned: *const u32) -> i32;
-    pub fn GetProcessImageFileNameW(process_handle: *const (), image_file_name: *const u16, image_size: u32) -> u32;
+    fn EnumProcesses(process_ids_list: *const u32, process_ids_list_size: u32, bytes_returned: *const u32) -> i32;
+    fn GetProcessImageFileNameW(process_handle: *const (), image_file_name: *const u16, image_size: u32) -> u32;
 }
 
 #[repr(C)]
@@ -35,12 +56,33 @@ pub struct Module
     pub size_of_image: usize,
     pub entry_point: usize
 }
+
 /// ## EN
 /// Structure of process allowing to interact with virtual memory.<br/>
 /// Contains process name, handle and ID (PID).
+/// #### Example:
+/// ```
+/// let name_process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+/// let id_process: Process = Process::new_from_id(12604).unwrap();
+///
+/// // Process { handle: 0x3f0, name: "cheatengine-x86_64.exe", id: 12604 }
+/// // Process { handle: 0x3f0, name: "cheatengine-x86_64.exe", id: 12604 }
+/// println!("{:?}", name_process);
+/// println!("{:?}", id_process);
+/// ```
 /// ## RU
 /// Структура процесса, позволяющая взаимодейстовать с виртуальной памятью.<br/>
 /// Содержит дескриптор, имя и ID (PID) процесса.
+/// #### Пример:
+/// ```
+/// let name_process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+/// let id_process: Process = Process::new_from_id(12604).unwrap();
+///
+/// // Process { handle: 0x3f0, name: "cheatengine-x86_64.exe", id: 12604 }
+/// // Process { handle: 0x3f0, name: "cheatengine-x86_64.exe", id: 12604 }
+/// println!("{:?}", name_process);
+/// println!("{:?}", id_process);
+/// ```
 #[repr(C)]
 #[derive(Debug, Clone)]
 pub struct Process
@@ -106,7 +148,7 @@ impl Process
                 }
                 None
             },
-            None => {None}
+            None => None
         }
     }
 
@@ -163,7 +205,7 @@ impl Process
                 }
                 None
             },
-            None => {None}
+            None => None
         }
     }
     /// ## EN
@@ -196,6 +238,176 @@ impl Process
 
             let is_64_check_successed: i32 = IsWow64Process(self.handle, &mut is_64_bit);
             if is_64_check_successed != 0 {Some(if is_64_bit != 0 {true} else {false})} else {None}
+        }
+    }
+
+    /// ## EN
+    /// Displays memory byte table to console like it's made in [`Cheat Engine`] memory viewer.</br>
+    /// * If 2nd argument is `0` sets it to default `8`.
+    /// * If 3rd argument is `0` sets it to default `16`.
+    /// 
+    /// Retrurns [`bool`] dependent on the success of the operation, table will be displayed if operation successed.
+    /// #### Example:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// 
+    /// // Console output:
+    /// // B2 B6 B2 40 6F 6A C4 9A 30 1F 33 A4 A5 01 DD 1F 
+    /// // 5B D9 69 E2 B8 50 32 F8 24 26 26 5A 60 3F AE 3D
+    /// // 56 4C A5 FA C6 C7 C7 5B C3 67 B8 EF A9 6B 6A 6A
+    /// // FE 06 E3 F5 98 36 86 E8 43 B2 77 AD 6D E2 3D FE
+    /// // 3B 8E 45 50 DB F6 61 A8 4D BB F6 C8 C5 4A 1F 95
+    /// // A7 FC 67 A7 EF 2C 71 43 B6 46 CA C8 48 43 91 B6
+    /// // 67 30 FF B2 BA 9B 25 6E 43 72 DF B4 B5 B5 CF E1
+    /// // 32 2D C5 85 C7 FE FD FB D5 B1 6F 38 44 57 57 F7
+    /// process.byte_table(0x07A500A4, 0, 0);
+    /// ```
+    /// ## RU
+    /// Отображает байт-таблицу памяти в консоле как это реализовано в обозреватели памяти [`Cheat Engine`].</br>
+    /// * Если 2-ой аргумент `0`, то устнавливается как `8` по умолчанию.
+    /// * Eсли 3-ий аргумент `0`, то устанавливается его как `16` по умолчанию.
+    /// 
+    /// Возвращает [`bool`], зависящий от успешности выполнения операции. Таблица будет выведена если операция прошла успешно.
+    /// #### Пример:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// 
+    /// // Console output:
+    /// // B2 B6 B2 40 6F 6A C4 9A 30 1F 33 A4 A5 01 DD 1F 
+    /// // 5B D9 69 E2 B8 50 32 F8 24 26 26 5A 60 3F AE 3D
+    /// // 56 4C A5 FA C6 C7 C7 5B C3 67 B8 EF A9 6B 6A 6A
+    /// // FE 06 E3 F5 98 36 86 E8 43 B2 77 AD 6D E2 3D FE
+    /// // 3B 8E 45 50 DB F6 61 A8 4D BB F6 C8 C5 4A 1F 95
+    /// // A7 FC 67 A7 EF 2C 71 43 B6 46 CA C8 48 43 91 B6
+    /// // 67 30 FF B2 BA 9B 25 6E 43 72 DF B4 B5 B5 CF E1
+    /// // 32 2D C5 85 C7 FE FD FB D5 B1 6F 38 44 57 57 F7
+    /// process.byte_table(0x07A500A4, 0, 0);
+    /// ```
+    /// [`Cheat Engine`]: https://www.cheatengine.org/aboutce.php
+    pub fn byte_table(&self, adress: usize, mut string_count: u32, mut string_length: u32) -> bool
+    {
+        if string_count == 0 {string_count = 8;}
+        if string_length == 0 {string_length = 16;}
+
+        let mut bytes_vector: Vec<u8> = Vec::new();
+
+        for offset in 0..(string_count * string_length) as usize
+        {
+            match self.read_memory::<u8>(adress + offset)
+            {
+                Some(byte) => bytes_vector.push(byte),
+                None => {}
+            }
+        }
+
+        if bytes_vector.len() == (string_count * string_length) as usize
+        {
+            let mut counter: usize = 0;
+            for _ in 0..string_count
+            {
+                for _ in 0..string_length
+                {
+                    print!("{}", &format!("{:#04X} ", bytes_vector[counter])[2..]);
+                    counter += 1;
+                }
+                print!("\n");
+            }
+            true
+        } else {false}
+    }
+
+    /// ## EN
+    /// Allocates memory in process.<br/>
+    /// * If 2nd argument is `0` sets it to default `4096`.
+    /// * if 3rd argument is `0` sets it to default `MEM_COMMIT`.
+    /// * If 4th argument is `0` sets it to default `PAGE_EXECUTE_READWRITE`.
+    /// 
+    /// Return [`Option`] containing [`usize`] adress of allocation if operation successed, oterwise returns [`None`].
+    /// #### Example:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let alloc: usize = process.alloc_memory(0, 2048, MEM_COMMIT, PAGE_EXECUTE_READWRITE).unwrap();
+    ///
+    /// // 0x2aa0000
+    /// println!("0x{alloc:x}");
+    /// ```
+    /// ## RU
+    /// Аллоцирует память в процессе.
+    /// * Если 2-ой аргумент `0`, то устнавливается как `4096` по умолчанию.
+    /// * Если 3-ой аргумент `0`, то устнавливается как `MEM_COMMIT` по умолчанию.
+    /// * Если 4-ой аргумент `0`, то устнавливается как `PAGE_EXECUTE_READWRITE` по умолчанию.
+    /// 
+    /// Возвращает [`Option`], содержащий [`usize`] адрес на выделенный участок памяти если операция прошла успешно, иначе возвращает [`None`].
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let alloc: usize = process.alloc_memory(0, 2048, MEM_COMMIT, PAGE_EXECUTE_READWRITE).unwrap();
+    ///
+    /// // 0x2aa0000
+    /// println!("0x{alloc:x}");
+    /// ```
+    pub fn alloc_memory(&self, desired_address: usize, mut allocation_size: usize, mut allocation_type: u32, mut protection: u32) -> Option<usize>
+    {
+        if allocation_size == 0 {allocation_size = 4096;}
+        if allocation_type == 0 {allocation_type = MEM_COMMIT;}
+        if protection == 0 {protection = PAGE_EXECUTE_READWRITE;}
+
+        unsafe
+        {
+            let allocation_region: *const () = VirtualAllocEx(self.handle, desired_address as _, allocation_size, allocation_type, protection);
+            if !allocation_region.is_null() {Some(allocation_region as usize)} else {None}
+        }
+    }
+
+    /// ## EN
+    /// Frees region with allocated memory in process.
+    /// * If 2nd argument is `0` frees memory of the entire page.
+    /// * if 3rd argument is `0` sets it to default `MEM_RELEASE`.
+    /// 
+    /// Returns [`bool`] dependent on the success of the operation.
+    /// #### Exmaple:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let alloc: usize = process.alloc_memory(0, 2048, 0, 0).unwrap();
+    ///
+    /// let free_successed: bool = process.free_memory(alloc, 0, 0);
+    ///
+    /// // true
+    /// println!("{free_successed}");
+    ///
+    /// let free_successed: bool = process.free_memory(alloc, 0, 0);
+    ///
+    /// // false (Trying to free already released memory)
+    /// println!("{free_successed}");
+    /// ```
+    /// ## RU
+    /// Высвобождает регион с аллоцированной памятью в процессе.
+    /// * Если 2-ой аргумент `0`, то высвобождает страницу полностью.
+    /// * Если 3-ой аргумент `0`, то устнавливается как `MEM_RELEASE` по умолчанию.
+    /// 
+    /// Возвращает [`bool`], зависящий от успешности выполнения операции.
+    /// #### Пример:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let alloc: usize = process.alloc_memory(0, 2048, 0, 0).unwrap();
+    ///
+    /// let free_successed: bool = process.free_memory(alloc, 0, 0);
+    ///
+    /// // true
+    /// println!("{free_successed}");
+    ///
+    /// let free_successed: bool = process.free_memory(alloc, 0, 0);
+    ///
+    /// // false (Trying to free already released memory)
+    /// println!("{free_successed}");
+    /// ```
+    pub fn free_memory(&self, allocation_address: usize, allocation_size: usize, mut free_type: u32) -> bool
+    {
+        if free_type == 0 {free_type = MEM_RELEASE};
+
+        unsafe
+        {
+            let memory_free_successed: i32 = VirtualFreeEx(self.handle, allocation_address as _, allocation_size, free_type);
+            if memory_free_successed != 0 {true} else {false}
         }
     }
 
@@ -317,12 +529,10 @@ pub fn list_processes() -> Option<Vec<(u32, String)>>
     {
         match EnumProcesses(&process_ids as _, std::mem::size_of_val(&process_ids) as _, &bytes_returned)
         {
-            0 => {None}
+            0 => None,
             _ =>
             {
-                let active_processes_count: usize = (bytes_returned / std::mem::size_of::<u32>() as u32) as _;
-
-                for process_id in 0..active_processes_count
+                for process_id in 0..(bytes_returned / std::mem::size_of::<u32>() as u32) as _
                 {
                     let process_handle: *const () = OpenProcess(PROCESS_ALL_ACCESS, 0, process_ids[process_id]);
 
