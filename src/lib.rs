5d492f95881c5443fa35464ba658617a58e060b8
Xenely14 <a.maryatkin14@gmail.com>
2023-05-20 04:48:18 +0800

    Added MemoryInformation struct, Process methods
    Addeme methods to "Process" struct:
        - list_modules
        - get_module
        - list_memory_pages
        - get_memory_page
    
    Added struct containing information about memory page - MemoryInformation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 const PATH_SIZE: usize = 512;
+const MODULE_NAME_SIZE: usize = 256;
 
 const PROCESS_ALL_ACCESS: u32 = 0x001F0FFF;
 
@@ -24,8 +25,14 @@ pub const PAGE_WRITECOPY: u32 = 0x008;
 pub const PAGE_EXECUTE_READ: u32 = 0x0020;
 pub const PAGE_EXECUTE_READWRITE: u32 = 0x0040;
 
+pub const LIST_MODULES_32BIT: u32 = 0x01;
+pub const LIST_MODULES_64BIT: u32 = 0x02;
+pub const LIST_MODULES_ALL: u32 = 0x03;
+pub const LIST_MODULES_DEFAULT: u32 = 0x0;
+
+#[allow(improper_ctypes)]
 #[link(name="kernel32")]
-extern "C"
+extern "system"
 {
     fn OpenProcess(desired_access: u32, inherit_handle: i32, process_id: u32) -> *const ();
     fn ReadProcessMemory(process_handle: *const (), read_address: *const (), buffer: *const (), buffer_size: usize, bytes_read: *const usize) -> i32;
@@ -33,28 +40,57 @@ extern "C"
     fn VirtualAllocEx(process_handle: *const (), desired_location: *const (), allocation_size: usize, allocation_type: u32, protection: u32) -> *const ();
     fn VirtualFreeEx(process_handle: *const (), allocation_address: *const (), allocation_size: usize, free_type: u32) -> i32;
     fn IsWow64Process(process_handle: *const (), is_64_bit: *const i32) -> i32;
+    fn VirtualQueryEx(process_handle: *const (), address: *const (), memory_info: *const MemoryInformation, bytes_length: usize) -> usize;
 }
 
+#[allow(improper_ctypes)]
 #[link(name="psapi")]
-extern "C"
+extern "system"
 {
     fn EnumProcesses(process_ids_list: *const u32, process_ids_list_size: u32, bytes_returned: *const u32) -> i32;
-    fn GetProcessImageFileNameW(process_handle: *const (), image_file_name: *const u16, image_size: u32) -> u32;
+    fn EnumProcessModulesEx(process_handle: *const (), modules_list: *const *const (), module_list_size: u32, bytes_returned: *const u32, filter: u32) -> i32;
+    fn GetProcessImageFileNameW(process_handle: *const (), name_buffer: *const u16, buffer_size: u32) -> u32;
+    fn GetModuleInformation(process_handle: *const (), module: *const (), module_info: *const Module, bytes_returned: u32) -> i32;
+    fn GetModuleBaseNameW(process_handle: *const (), module: *const (), name_buffer: *const u16, buffer_size: u32) -> u32;
 }
 
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
 /// ## EN
 /// Structure of process module.<br/>
-/// Contains module base address, size of image and entry point.
+/// Contains module base address, size of image, entry point and name.
 /// ## RU
 /// Структура модуля процесса.<br/>
-/// Содержит базовый адрес, размер изображения и точку входа модуля.
+/// Содержит базовый адрес, размер изображения, точку входа и имя модуля.
+#[repr(C)]
+#[derive(Debug)]
 pub struct Module
 {
-    pub base_address: usize,
-    pub size_of_image: usize,
-    pub entry_point: usize
+    pub base_address: *const (),
+    pub size_of_image: u32,
+    pub entry_point: *const (),
+
+    // Local attributes
+    pub name: String
+}
+
+/// ## EN
+/// Structure of process memory information.<br/>
+/// Contains information about memory page in process.
+/// ## RU
+/// Структура информации памяти процесса.<br/>
+/// Содержит информацию о участке памяти в процессе.
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+pub struct MemoryInformation
+{
+    pub base_address: u64,
+    pub allocation_base: u64,
+    pub allocation_protection: u32,
+    pub alignment_1: u32,
+    pub region_size: u64,
+    pub state: u32,
+    pub protect: u32,
+    pub memory_type: u32,
+    pub alignment_2: u32
 }
 
 /// ## EN
@@ -84,7 +120,7 @@ pub struct Module
 /// println!("{:?}", id_process);
 /// ```
 #[repr(C)]
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Process
 {
     pub handle: *const (),
@@ -94,6 +130,7 @@ pub struct Process
 
 impl Process
 {
+
     /// ## EN
     /// Constructs process object from process ID.<br/>
     /// Returns [`Option`] containing `Process` object if operation successed, otherwise returns [`None`].
@@ -140,6 +177,7 @@ impl Process
     {
         match list_processes()
         {
+            None => None,
             Some(processes) =>
             {
                 for (id, name) in processes
@@ -147,8 +185,7 @@ impl Process
                     if process_id == id {return Some(Self{handle: unsafe{OpenProcess(PROCESS_ALL_ACCESS, 0, id)}, name, id})}
                 }
                 None
-            },
-            None => None
+            }
         }
     }
 
@@ -197,6 +234,7 @@ impl Process
     {
         match list_processes()
         {
+            None => None,
             Some(processes) =>
             {
                 for (id, name) in processes
@@ -204,8 +242,7 @@ impl Process
                     if name.to_lowercase().trim() == process_name.to_lowercase().trim() {return Some(Self{handle: unsafe{OpenProcess(PROCESS_ALL_ACCESS, 0, id)}, name, id})}
                 }
                 None
-            },
-            None => None
+            }
         }
     }
     /// ## EN
@@ -284,7 +321,7 @@ impl Process
     /// process.byte_table(0x07A500A4, 0, 0);
     /// ```
     /// [`Cheat Engine`]: https://www.cheatengine.org/aboutce.php
-    pub fn byte_table(&self, adress: usize, mut string_count: u32, mut string_length: u32) -> bool
+    pub fn byte_table(&self, address: usize, mut string_count: u32, mut string_length: u32) -> bool
     {
         if string_count == 0 {string_count = 8;}
         if string_length == 0 {string_length = 16;}
@@ -293,10 +330,10 @@ impl Process
 
         for offset in 0..(string_count * string_length) as usize
         {
-            match self.read_memory::<u8>(adress + offset)
+            match self.read_memory::<u8>(address + offset)
             {
-                Some(byte) => bytes_vector.push(byte),
-                None => {}
+                None => {},
+                Some(byte) => bytes_vector.push(byte)
             }
         }
 
@@ -322,7 +359,7 @@ impl Process
     /// * if 3rd argument is `0` sets it to default `MEM_COMMIT`.
     /// * If 4th argument is `0` sets it to default `PAGE_EXECUTE_READWRITE`.
     /// 
-    /// Return [`Option`] containing [`usize`] adress of allocation if operation successed, oterwise returns [`None`].
+    /// Return [`Option`] containing [`usize`] address of allocation if operation successed, otherwise returns [`None`].
     /// #### Example:
     /// ```
     /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
@@ -408,6 +445,206 @@ impl Process
         {
             let memory_free_successed: i32 = VirtualFreeEx(self.handle, allocation_address as _, allocation_size, free_type);
             if memory_free_successed != 0 {true} else {false}
+        }
+    }
+
+    /// ## EN
+    /// Finds and gathers process modules matching the filter.<br/>
+    /// Returns [`Option`] containing [`Vec`] with `Module`objects if operation successed, otherwise returns [`None`].
+    /// #### Example:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let modules: Vec<Module> = process.list_modules(LIST_MODULES_ALL).unwrap();
+    /// 
+    /// // 64
+    /// println!("{}", modules.len());
+    /// 
+    /// // Module { base_address: 0x400000, size_of_image: 17895424, entry_point: 0x403de0, name: "cheatengine-x86_64.exe" }
+    /// // Module { base_address: 0x7ffabfa90000, size_of_image: 2179072, entry_point: 0x0, name: "ntdll.dll" }
+    /// // ...
+    /// // Module { base_address: 0x7ffab9e90000, size_of_image: 1769472, entry_point: 0x7ffab9ef7ea0, name: "WindowsCodecs.dll" }
+    /// for module in modules
+    /// {
+    ///     println!{"{module:?}"};
+    /// }
+    /// ```
+    /// ## RU
+    /// Находит и собирает модули процесса соответствующие фильтру.<br/>
+    /// Возвращает [`Option`], содержащий [`Vec`] с объектами `Module` если операция прошла успешно, иначе возвращает [`None`].
+    /// #### Пример:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let modules: Vec<Module> = process.list_modules(LIST_MODULES_ALL).unwrap();
+    /// 
+    /// // 64
+    /// println!("{}", modules.len());
+    /// 
+    /// // Module { base_address: 0x400000, size_of_image: 17895424, entry_point: 0x403de0, name: "cheatengine-x86_64.exe" }
+    /// // Module { base_address: 0x7ffabfa90000, size_of_image: 2179072, entry_point: 0x0, name: "ntdll.dll" }
+    /// // ...
+    /// // Module { base_address: 0x7ffab9e90000, size_of_image: 1769472, entry_point: 0x7ffab9ef7ea0, name: "WindowsCodecs.dll" }
+    /// for module in modules
+    /// {
+    ///     println!{"{module:?}"};
+    /// }
+    /// ```
+    pub fn list_modules(&self, filter: u32) -> Option<Vec<Module>>
+    {
+        let mut modules_vec: Vec<Module> = Vec::new();
+
+        let process_modules: [*const (); 4096] = [0 as _; 4096];
+        let bytes_returned: u32 = 0;
+
+        unsafe
+        {
+            match EnumProcessModulesEx(self.handle, &process_modules as _, std::mem::size_of_val(&process_modules) as _, &bytes_returned, filter)
+            {
+                0 => None,
+                _ =>
+                {
+                    for module_id in 0..(bytes_returned / std::mem::size_of::<*const ()>() as u32) as _
+                    {
+                        let mut current_module: Module = Module{base_address: 0 as _, size_of_image: 0, entry_point: 0 as _, name: "".to_string()};
+
+                        match GetModuleInformation(self.handle, process_modules[module_id], &mut current_module as _, std::mem::size_of::<Module>() as _)
+                        {
+                            0 => {},
+                            _ => 
+                            {
+                                let module_name_raw: [u16; MODULE_NAME_SIZE] = [0; MODULE_NAME_SIZE];
+                                GetModuleBaseNameW(self.handle, current_module.base_address, &module_name_raw as _, std::mem::size_of_val(&module_name_raw) as _);
+
+                                let module_name: String = String::from_utf16_lossy(&module_name_raw).trim_end_matches('\0').to_string();
+                                if !module_name.is_empty()
+                                {
+                                    current_module.name = module_name;
+                                    modules_vec.push(current_module);
+                                }
+                            }
+                        }
+                    }
+                    Some(modules_vec)
+                }
+            }
+        }
+    }
+
+    /// ## EN
+    /// Gets information about module in process.<br/>
+    /// Returns [`Option`] containing `Module` object if operation successed, otherwise returns [`None`].
+    /// #### Example:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let kernel_module: Module = process.get_module("kernel32.dll").unwrap();
+    ///
+    /// // Module { base_address: 0x7fff967f0000, size_of_image: 794624, entry_point: 0x7fff96802710, name: "KERNEL32.DLL" }
+    /// println!("{:?}", kernel_module);
+    /// ```
+    /// ## RU
+    /// Получает информацю о модуле в процессe.<br/>
+    /// Возвращает [`Option`], содержащий объект `Module` в cлучае успешной операции, в противном случае возвращает [`None`].
+    /// #### Пример:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let kernel_module: Module = process.get_module("kernel32.dll").unwrap();
+    ///
+    /// // Module { base_address: 0x7fff967f0000, size_of_image: 794624, entry_point: 0x7fff96802710, name: "KERNEL32.DLL" }
+    /// println!("{:?}", kernel_module);
+    /// ```
+    pub fn get_module(&self, module_name: &str) -> Option<Module>
+    {
+        match self.list_modules(LIST_MODULES_ALL)
+        {
+            None => None,
+            Some(modules) => 
+            {
+                for module in modules
+                {
+                    if module.name.to_lowercase().trim() == module_name.to_string().to_lowercase().trim() {return Some(module)}
+                }
+                None
+            }
+        }
+    }
+
+    /// ## EN
+    /// Finds and gathers process memory pages.<br/>
+    /// Returns [`Option`] containing [`Vec`] with `MemoryInformation`objects if operation successed, otherwise returns [`None`].
+    /// #### Example:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let memory_pages: Vec<MemoryInformation> = process.list_memory_pages().unwrap();
+    ///
+    /// // MemoryInformation { base_address: 118947840, allocation_base: 118947840, allocation_protection: 2, alignment_1: 0, region_size: 90112, state: 4096, protect: 2, memory_type: 262144, alignment_2: 0 } 
+    /// // MemoryInformation { base_address: 119037952, allocation_base: 0, allocation_protection: 0, alignment_1: 0, region_size: 40960, state: 65536, protect: 1, memory_type: 0, alignment_2: 0 }
+    /// // ...
+    /// // MemoryInformation { base_address: 190906368, allocation_base: 190906368, allocation_protection: 4, alignment_1: 0, region_size: 4096, state: 4096, protect: 4, memory_type: 131072, alignment_2: 0 }   
+    /// for memory_page in memory_pages
+    /// {
+    ///     println!("{memory_page:?}");
+    /// }
+    /// ```
+    /// ## RU
+    /// Находит и собирает участки памяти в процессе.<br/>
+    /// Возвращает [`Option`], содержащий [`Vec`] с объектами `Module` если операция прошла успешно, иначе возвращает [`None`].
+    /// #### Пример:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let memory_pages: Vec<MemoryInformation> = process.list_memory_pages().unwrap();
+    ///
+    /// // MemoryInformation { base_address: 118947840, allocation_base: 118947840, allocation_protection: 2, alignment_1: 0, region_size: 90112, state: 4096, protect: 2, memory_type: 262144, alignment_2: 0 } 
+    /// // MemoryInformation { base_address: 119037952, allocation_base: 0, allocation_protection: 0, alignment_1: 0, region_size: 40960, state: 65536, protect: 1, memory_type: 0, alignment_2: 0 }
+    /// // ...
+    /// // MemoryInformation { base_address: 190906368, allocation_base: 190906368, allocation_protection: 4, alignment_1: 0, region_size: 4096, state: 4096, protect: 4, memory_type: 131072, alignment_2: 0 }   
+    /// for memory_page in memory_pages
+    /// {
+    ///     println!("{memory_page:?}");
+    /// }
+    /// ```
+    pub fn list_memory_pages(&self) -> Option<Vec<MemoryInformation>>
+    {
+        let mut memory_pages_vec: Vec<MemoryInformation> = Vec::new();
+        
+        let mut memory_page_info: Option<MemoryInformation> = self.get_memory_page(0);
+        while let Some(memory_info) = memory_page_info
+        {
+            memory_pages_vec.push(memory_info);
+            memory_page_info = self.get_memory_page(memory_info.base_address as usize + memory_info.region_size as usize);
+        }
+
+        if memory_pages_vec.len() != 0 {Some(memory_pages_vec)} else {None}
+    }
+
+    /// ## EN
+    /// Gets information about memory page in process.<br/>
+    /// Returns [`Option`] containing `MemoryInformation` object if operation successed, otherwise returns [`None`].
+    /// #### Example:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let process_base_memory_info: MemoryInformation = process.get_memory_page(0x10000).unwrap();
+    ///
+    /// // MemoryInformation { base_address: 65536, allocation_base: 65536, allocation_protection: 4, alignment_1: 0, region_size: 65536, state: 4096, protect: 4, memory_type: 262144, alignment_2: 0 }
+    /// println!("{:?}", process_base_memory_info);
+    /// ```
+    /// ## RU
+    /// Получает информацю об участке памяти в процессe.<br/>
+    /// Возвращает [`Option`], содержащий объект `MemoryInformation` в cлучае успешной операции, в противном случае возвращает [`None`].
+    /// #### Пример:
+    /// ```
+    /// let process: Process = Process::new_from_name("cheatengine-x86_64.exe").unwrap();
+    /// let process_base_memory_info: MemoryInformation = process.get_memory_page(0x10000).unwrap();
+    ///
+    /// // MemoryInformation { base_address: 65536, allocation_base: 65536, allocation_protection: 4, alignment_1: 0, region_size: 65536, state: 4096, protect: 4, memory_type: 262144, alignment_2: 0 }
+    /// println!("{:?}", process_base_memory_info);
+    /// ```
+    pub fn get_memory_page(&self, address: usize) -> Option<MemoryInformation>
+    {
+        let mut memory_information: MemoryInformation = MemoryInformation {base_address: 0, allocation_base: 0, allocation_protection: 0, alignment_1: 0, region_size: 0, state: 0, protect: 0, memory_type: 0, alignment_2: 0};
+
+        unsafe
+        {
+            let virtual_querry_successsed: usize = VirtualQueryEx(self.handle, address as _, &mut memory_information as _, std::mem::size_of::<MemoryInformation>());
+            if virtual_querry_successsed != 0 {Some(memory_information)} else {None}
         }
     }
 
@@ -542,8 +779,8 @@ pub fn list_processes() -> Option<Vec<(u32, String)>>
                     let file_name: String = String::from_utf16_lossy(&file_name_raw).trim_end_matches('\0').to_string();
                     match file_name.split("\\").last()
                     {
-                        Some(name) => {if !name.is_empty() {processes_vec.push((process_ids[process_id], name.to_string()))}},
-                        None => {}
+                        None => {},
+                        Some(name) => {if !name.is_empty() {processes_vec.push((process_ids[process_id], name.to_string()))}}
                     }
                 }
                 Some(processes_vec)
